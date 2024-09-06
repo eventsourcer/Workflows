e6d209b024cf122b2aa20e097bde6c9d63815f14
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-09-06 15:24:38 +0300

    add tests

--- Workflows.Tests/UnitTest1.cs
+++ Workflows.Tests/UnitTest1.cs
@@ -1,4 +1,12 @@
-﻿namespace Workflows.Tests;
+﻿using System.Diagnostics.Tracing;
+using AsyncHandler.EventSourcing.Configuration;
+using AsyncHandler.EventSourcing.Repositories.AzureSql;
+using AsyncHandler.EventSourcing.SourceConfig;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Workflows.Tests;
 
 public class UnitTest1
 {
@@ -12,5 +20,36 @@ public class UnitTest1
     {
         List<int> folks = [1, 2, 3];
         Assert.Equal(3, folks.Count);
+    }
+    [Fact]
+    public async Task TestAzureSqlClient()
+    {
+        // giveb
+        var sp = BuildCOntainer();
+        var conn = BuildConfiguration();
+        var client = new AzureSqlClient<OrderAggregate>(conn, sp, EventSources.AzureSql);
+
+        //when
+        var aggregate = await client.CreateOrRestore();
+
+        // then
+        Assert.NotNull(aggregate);
+    }
+    private IServiceProvider BuildCOntainer()
+    {
+        var services = new ServiceCollection();
+        var factory = new LoggerFactory();
+        services.AddSingleton<ILogger<AzureSqlClient<OrderAggregate>>>(new Logger<AzureSqlClient<OrderAggregate>>(factory));
+        Dictionary<EventSources,IClientConfig> configs = new();
+        configs.Add(EventSources.AzureSql, new AzureSqlConfig());
+        services.AddKeyedSingleton("SourceConfig", configs);
+        return services.BuildServiceProvider();
+    }
+    private static string BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddUserSecrets<UnitTest1>();
+        return builder.Build().GetValue<string>("AzureSqlDatabase") ??
+        throw new Exception("no connection string found");
     }
 }
