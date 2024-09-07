2f5c3c1f99fa00b6297b4e02fe71d60dc9bd75d4
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-09-07 15:13:02 +0300

    remove envs

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 
 echo "Creating asynchandler database..................."
 
-echo "checking for azuresqlenv: $azuresqlenv"
-echo "checking for mssqlenv: $mssqlenv"
+echo "checking for azuresqlenv: azuresqlenv"
+echo "checking for mssqlenv: mssqlenv"
 
 
 # sqlcmd -s localhost -U sa -P ${{ env.mssql_password }} -q "create database ${{ env.mssql_db }};"
