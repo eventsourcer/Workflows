f9edf3bb185838eb5b2d6e8cff99e88a50d7d296
Sarwan Surchi <sarwan.h.najim@gmail.com>
2024-09-07 15:41:00 +0300

    create db

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ echo "Creating asynchandler database..................."
 echo "checking for azuresqlenv: $azuresqlenv"
 echo "checking for mssqlenv: $mssqlenv"
 
+echo "running script for mssql db: $mssql_db"
 
-# sqlcmd -s localhost -U sa -P ${{ env.mssql_password }} -q "create database ${{ env.mssql_db }};"
+sqlcmd -s localhost -U sa -P $mssql_password -q "create database $mssql_db;"
+
+echo "script completed successfully...."
